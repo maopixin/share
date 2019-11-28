afb972ba6d160e6082e480f95979d78b0dc43601
oliver <18364076997@163.com>
2019-11-28 14:37:47 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { sayHello } from "./gree";
+function showHello(divName: string, name: string) {
+    const elt = document.getElementById(divName);
+    elt.innerText = sayHello(name);
+}
+showHello("greeting", "TypeScript");
