e534756ca2b0a6e97640b66a12b4bfb7586347a0
oliver <18364076997@163.com>
2019-12-20 14:11:14 +0800

    fix: 完善函数注释

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
+/*
+ * @Author: oliver
+ * @Date: 2019-11-28 13:51:50
+ * @LastEditTime : 2019-12-20 14:10:45
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /share/src/main.ts
+ */
 import defaults from './data/options';
 import { getDataSet } from './utils';
 import './style/index.less'
+
 class Share {
+
     el: string;
     config: object;
+
     constructor(el:string = '.custom-share', config:object = {}) {
         const dataConfig = getDataSet(el);
         this.config = Object.assign(defaults, dataConfig, config);
         this.initialization();
     };
+
     initialization() {
         console.log(this.config);
     };
+
 };
+
 export default Share;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,35 @@
+/*
+ * @Author: oliver
+ * @Date: 2019-12-03 09:54:02
+ * @LastEditTime : 2019-12-20 14:10:13
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /share/src/utils/index.ts
+ */
+/**
+ * @description: 通过meta标签的name获取meta标签的内容
+ * @param {string} name meta标签的名字 
+ * @return {string}: meta标签的内容
+ */
 export function getMetaContentByName(name: string):string {
     const ele = <HTMLMetaElement>document.getElementsByName(name)[0];
     return ele ? ele.content : '';
 }
 
+/**
+ * @description: 获取html文档里面的第一张图片
+ * @return: html文档里面的第一张图片的地址
+ */
 export function getFirstImage():string {
     const ele = <HTMLImageElement>document.images[0];
     return ele ? ele.src : '';
 }
 
+/**
+ * @description: 通过class获取元素的自定义属性对象
+ * @param {string}} cls 元素的class（.class）
+ * @return {object}: 元素的data-属性
+ */
 export function getDataSet(cls:string):object {
     const ele = <HTMLElement>document.querySelector(cls);
     
